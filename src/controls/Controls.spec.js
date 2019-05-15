2999ba6d7f61a1822f318f0eff175c4f2302d830
matt-poloni <46501814+matt-poloni@users.noreply.github.com>
2019-05-15 16:58:21 -0400

    Tests if buttons exist to change each state

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -1,9 +1,26 @@
 import React from 'react';
+import { render } from 'react-testing-library';
 import 'react-testing-library/cleanup-after-each';
 import Controls from './Controls';
 
-describe.skip('<Controls />', () => {
+describe('<Controls />', () => {
   it('should render', () => {
     render(<Controls />);
+  })
+
+  describe('Close/Open Gate button', () => {
+    it("should provide a button to toggle the 'closed' state", () => {
+      const { getByText } = render(<Controls />);
+      getByText(/Close Gate/);
+    })
+
+  })
+
+  describe('Lock/Unlock Gate button', () => {
+    it("should provide a button to toggle the 'locked' state", () => {
+      const { getByText } = render(<Controls />);
+      getByText(/Lock Gate/);
+    })
+
   })
 })

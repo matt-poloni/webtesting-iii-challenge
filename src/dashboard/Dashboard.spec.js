857f81827e58015647f46f24caeb9dfb0f7be52d
matt-poloni <46501814+matt-poloni@users.noreply.github.com>
2019-05-15 17:46:18 -0400

    Adds snapshot test

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import renderer from 'react-test-renderer';
 import { render } from 'react-testing-library';
 import 'react-testing-library/cleanup-after-each';
 import Dashboard from './Dashboard';
@@ -7,6 +8,11 @@ describe('<Dashboard />', () => {
   it('should render', () => {
     render(<Dashboard />);
   })
+
+  it('should match snapshot', () => {
+    const tree = renderer.create(<Dashboard />).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
 
   it('should display <Controls />', () => {
     const { getByText } = render(<Dashboard />);
